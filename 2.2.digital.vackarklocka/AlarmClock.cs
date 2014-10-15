92102aae1896a68104726585bce70bde47c75ec1
mm222qy <mm222qy@student.lnu.se>
2014-10-15 14:05:17 +0200

    finished

---
+++ 2.2.digital.vackarklocka/AlarmClock.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2.digital.vackarklocka
+{
+    class AlarmClock
+    {
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException();
+                }
+                _alarmHour = value;
+            }
+        }
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException();
+                }
+                _alarmMinute = value;
+            }
+        }
+        public int Hour
+        {
+            get { return _hour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException();
+                }
+                _hour = value;
+            }
+        }
+        public int Minute
+        {
+            get { return _minute; }
+            set
+            {
+
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException();
+                }
+                _minute = value;
+            }
+        }
+
+        
+        public AlarmClock()
+            : this(0, 0)
+        {
+            
+        }
+
+        public AlarmClock(int hour, int minute)
+            : this(hour, minute, 0, 0)
+        {
+            
+        }
+
+        
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+            Hour = hour;
+            Minute = minute;
+            AlarmHour = alarmHour;
+            AlarmMinute = alarmMinute;
+        }
+
+        
+
+        public bool TickTock()
+        {
+            
+            if (Minute < 59)
+            {
+                ++Minute;
+            }
+            else
+            {
+                Minute = 0;
+
+                if (Hour < 23)
+                {
+                    Hour++;
+                }
+                else
+                {
+                    Hour = 0;
+                }
+            }
+            if (Hour == AlarmHour && Minute == AlarmMinute)
+            {
+                return true;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            return string.Format(" {0,4}:{1:00} <{2}:{3:00}>", Hour, Minute, AlarmHour, AlarmMinute);
+        }
+    }
+}
---
+++ 2.2.digital.vackarklocka/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2.digital.vackarklocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ViewTestHeader("Test 1.\nTest av standardkonstruktorn");
+            AlarmClock Clock1 = new AlarmClock();
+            Console.WriteLine(Clock1.ToString());
+
+            ViewTestHeader("Test 2.\nTest av konstruktor med två parametrar, <9, 42>");
+            AlarmClock Clock2 = new AlarmClock(9, 42);
+            Console.WriteLine(Clock2.ToString());
+
+            ViewTestHeader("Test 3.\nTest av konstruktor med fyra parametrar, <13, 24, 7, 35>");
+            AlarmClock Clock3 = new AlarmClock(13, 24, 7, 35);
+            Console.WriteLine(Clock3.ToString());
+
+            ViewTestHeader("Test 4.\n Ställer befintligt AlarmClock-objekt till 23:58 och låter den gå 13 minuter.");
+            Clock3.Hour = 23;
+            Clock3.Minute = 58;
+            run(Clock3, 13);
+
+            ViewTestHeader("Test 5.\nStäller in befintligt AlarmClock-objet till tiden 6:12 och alarmtiden till 6:15 och låter tiden gå 6 minuter.");
+            Clock3.Hour = 6;
+            Clock3.Minute = 12;
+            Clock3.AlarmHour = 6;
+            Clock3.AlarmMinute = 15;
+            run(Clock3, 6);
+
+            ViewTestHeader("Test 6.\nTestar egenskaperna så att undantag kastas då tid och alarmtid tillsdelas felaktiga värden.");
+            try
+            {
+                Clock3.Hour = 24;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Timmen är inte i intervallet 0-23.");
+            }
+            try
+            {
+                Clock3.Minute = 60;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Minuten är inte i intervallet 0-59");
+            }
+            try
+            {
+                Clock3.AlarmHour = 24;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Alarmtimmen är inte i intervallet 0-23.");
+            }
+            try
+            {
+                Clock3.AlarmMinute = 60;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Alarmminuten är inte i intervallet 0-59");
+            }
+
+            ViewTestHeader("Test 7.\nTestar konstruktorer så att undantag kastas då tid och alarmtid tilldelas felaktiva värden.");
+            try
+            {
+                AlarmClock Clock4 = new AlarmClock(24, 0);
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Timmen är inte i intervallet 0-23.");
+            }
+            
+            try
+            {
+                AlarmClock Clock5 = new AlarmClock(0, 0, 24, 0);
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Alarmtimmen är inte i intervallet 0-23");
+            }
+            
+
+        }
+        private static void run(AlarmClock clock,int minute)
+        {
+             Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ╔══════════════════════════════════════╗ ");
+            Console.WriteLine(" ║       Väckarklockan URLED <TM>       ║ "); 
+            Console.WriteLine(" ║        Modellnr.:1DV402S2L2A         ║ ");
+            Console.WriteLine(" ╚══════════════════════════════════════╝ ");
+            Console.ResetColor();
+            
+            for (int i = 0; i < minute; i++)
+            { 
+                if (clock.TickTock() == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("♫" + clock.ToString() + " BEEP, BEEP, BEEP!!!");
+                }
+                else
+                {
+                    Console.ResetColor(); 
+                    Console.WriteLine(" " + clock.ToString());
+                }      
+            }
+        }
+        private static void ViewErrorMessage(string str)
+        { 
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(str);
+            Console.ResetColor();
+        }
+        private static void ViewTestHeader(string head)
+        {
+            Console.WriteLine("════════════════════════════════════════════════════════════════════════════");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(head);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+}
